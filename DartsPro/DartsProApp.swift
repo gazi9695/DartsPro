fae53e751091c9b8d2e30988dc03232396de2fcc
GoCloud <admin@gocloud.al>
2026-02-05 14:22:01 +0000

    Initial Commit

---
+++ DartsPro/DartsProApp.swift
@@ -0,0 +1,32 @@
+//
+//  DartsProApp.swift
+//  DartsPro
+//
+//  Created by Gazmir Cani on 05/02/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DartsProApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
